572370fb770c3a59911c5b8431195061dc6b9942
e-gloo <dcoodien@gmail.com>
2025-09-18 16:44:55 +0200

    feat: panel with save and restore buttons

---
+++ src/components/Controls.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { Panel, useVueFlow } from '@vue-flow/core'
+import Icon from './Icon.vue'
+
+const flowKey = 'vue-flow-project'
+
+const { nodes, addNodes, dimensions, toObject, fromObject } = useVueFlow(flowKey)
+
+function onSave() {
+  localStorage.setItem(flowKey, JSON.stringify(toObject()))
+}
+
+function onRestore() {
+  const flow = JSON.parse(localStorage.getItem(flowKey))
+
+  if (flow) {
+    fromObject(flow)
+  }
+}
+
+</script>
+
+<template>
+  <Panel position="top-right">
+    <div class="buttons">
+      <button title="save graph" @click="onSave">
+        <Icon name="save" />
+      </button>
+      <button title="restore graph" @click="onRestore">
+        <Icon name="restore" />
+      </button>
+    </div>
+  </Panel>
+</template>
--- src/components/FormBuilder.vue
+++ src/components/FormBuilder.vue
@@ -3,6 +3,7 @@ import { ref, onMounted } from 'vue'
 import type { Node, Edge } from '@vue-flow/core'
 import { VueFlow, useVueFlow } from '@vue-flow/core'
 import { Background } from '@vue-flow/background'
+import SaveRestoreControls from './Controls.vue'
 import { useFormBuilder } from '../composables/useFormBuilder'
 import { useDragAndDrop } from '../composables/useDnD'
 
@@ -31,6 +32,7 @@ onMounted(() => {
       <template #node-choice="choiceNodeProps">
         <ChoiceNode v-bind="choiceNodeProps" />
       </template>
+      <SaveRestoreControls />
       <Background variant="lines" />
     </VueFlow>
   </div>
@@ -53,5 +55,42 @@ onMounted(() => {
   border-width: 2px;
   border-color: oklch(35.519% 0.032 262.988);
   background-color: oklch(61.302% 0.202 261.294);
+}
+
+.vue-flow__panel {
+    background-color:#2d3748;
+    padding:10px;
+    border-radius:8px;
+    box-shadow:0 0 10px #00000080
+}
+
+.vue-flow__panel .buttons {
+    display:flex;
+    gap:8px
+}
+
+.vue-flow__panel button {
+    border:none;
+    cursor:pointer;
+    background-color:#4a5568;
+    border-radius:8px;
+    color:#fff;
+    box-shadow:0 0 10px #0000004d;
+    width:40px;
+    height:40px;
+    font-size:16px;
+    display:flex;
+    align-items:center;
+    justify-content:center
+}
+
+.vue-flow__panel button:hover {
+    background-color:#2563eb;
+    transition:background-color .2s
+}
+
+.vue-flow__panel button svg {
+    width:100%;
+    height:100%
 }
 </style>
