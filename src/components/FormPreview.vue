4900d17a09d1af81dae019c0f8030d4bf2facc52
e-gloo <dcoodien@gmail.com>
2026-02-15 18:07:46 +0100

    backup

--- src/components/FormPreview.vue
+++ src/components/FormPreview.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 
+const flowKey = 'vue-flow-project'
+onMounted(() => {
+  const flow = JSON.parse(localStorage.getItem(flowKey))
+});
 </script>
+
 
 <template>
   <div>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import FormBuilderPage from '../pages/FormBuilderPage.vue';
-import FormPage from '../pages/FormPage.vue';
+import FormPreviewPage from '../pages/FormPreviewPage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: FormBuilderPage },
-    { path: '/form', component: FormPage },
+    { path: '/form', component: FormPreviewPage },
   ],
 })
 
